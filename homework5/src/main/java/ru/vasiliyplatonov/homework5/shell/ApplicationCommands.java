74b682d40a8a40a5dcc01f313aa4b5cbd4ddb117
Vasiliy Platonov <vasiliyplat@gmail.com>
2021-11-13 20:34:23 +0300

    feat: add shell commands and ability to display as tables

---
+++ homework5/src/main/java/ru/vasiliyplatonov/homework5/shell/ApplicationCommands.java
@@ -0,0 +1,121 @@
+package ru.vasiliyplatonov.homework5.shell;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import ru.vasiliyplatonov.homework5.domain.Author;
+import ru.vasiliyplatonov.homework5.domain.Book;
+import ru.vasiliyplatonov.homework5.domain.Genre;
+import ru.vasiliyplatonov.homework5.service.BookService;
+import ru.vasiliyplatonov.homework5.shell.table.renderer.TableRenderer;
+
+import javax.validation.constraints.NotBlank;
+
+@ShellComponent
+@RequiredArgsConstructor
+public class ApplicationCommands {
+
+	private final BookService bookService;
+	private final TableRenderer<Book> bookTableRenderer;
+
+
+	@ShellMethod(value = "Show all books", key = {"book show"}, group = "book", prefix = "-")
+	public String showAllBooks(@ShellOption(help = "Possible values are: '-id', '-genre', '-author'", defaultValue = "all") String filterName,
+	                           @ShellOption(defaultValue = "") String filterValue) {
+
+		switch (filterName) {
+			case "all": {
+				return bookTableRenderer.render(
+						bookService.getAll());
+			}
+			case "-id": {
+				return bookTableRenderer.render(
+						bookService.getById(Long.parseLong(filterValue)));
+			}
+			case "-genre": {
+				return bookTableRenderer.render(
+						bookService.getByGenreName(filterValue));
+			}
+			case "-author": {
+				val fullNameArr = filterValue.split(" ");
+				val firstName = fullNameArr[0];
+				val lastName = fullNameArr[1];
+
+				return bookTableRenderer.render(
+						bookService.getByAuthorFirstNameAndLastName(firstName, lastName));
+			}
+			default:
+				return "Unsupported.\nUsing: book show [-id|-genre|-author <filterValue>]";
+		}
+	}
+
+	/**
+	 * Using: book add -title "The Playbook" -a-name "Barny" -a-surname "Stinson" -genre "tutorial"
+	 */
+	@ShellMethod(value = "Add book", key = {"book add"}, group = "book", prefix = "-")
+	public String addBook(@NotBlank @ShellOption(value = {"-t", "-title"}, defaultValue = "") String title,
+	                      @NotBlank @ShellOption(value = {"-a-name", "-author-firstname"}, defaultValue = "") String authorFirstName,
+	                      @NotBlank @ShellOption(value = {"-a-lname", "-author-lastname"}, defaultValue = "") String authorLastName,
+	                      @NotBlank @ShellOption(value = {"-g", "-genre"}, defaultValue = "") String genre) {
+
+		var book = new Book(0,
+				title,
+				new Author(0, authorFirstName, authorLastName),
+				new Genre(0, genre)
+		);
+
+		try {
+			val resultBook = bookService.add(book);
+			return resultBook.toString();
+		} catch (DuplicateKeyException e) {
+			return "The book already exists";
+		}
+
+	}
+
+
+	@ShellMethod(value = "Delete book", key = {"book delete"}, group = "book", prefix = "-")
+	public String deleteBook(@ShellOption(help = "Possible values are: '-id', '-title', '-author'", defaultValue = "-title") String filterName,
+	                         String filterValue) {
+
+		switch (filterName) {
+			case "-title": {
+				bookService.deleteByTitle(filterValue);
+			}
+			break;
+
+			case "-id": {
+				bookService.deleteById(Long.parseLong(filterValue));
+			}
+			break;
+
+			case "-author": {
+				val fullNameArr = filterValue.split(" ");
+				val firstName = fullNameArr[0];
+				val lastName = fullNameArr[1];
+
+				bookService.deleteByAuthor(new Author(0, firstName, lastName));
+			}
+			break;
+
+			default:
+				return "Unsupported.\nUsing: book delete [-id|-title|-author <filterValue>]";
+		}
+		return "Success";
+	}
+
+	@ShellMethod(value = "Update book", key = {"book update"}, group = "book", prefix = "-")
+	public String updateBook(@NotBlank String id,
+	                         @ShellOption(value = {"-t", "-title"}, defaultValue = "") String title,
+	                         @ShellOption(value = {"-a-name", "-author-firstname"}, defaultValue = "") String authorFirstName,
+	                         @ShellOption(value = {"-a-lname", "-author-lastname"}, defaultValue = "") String authorLastName,
+	                         @ShellOption(value = {"-g", "-genre"}, defaultValue = "") String genre) {
+
+		bookService.update(Long.parseLong(id), title, authorFirstName, authorLastName, genre);
+		return "Success";
+	}
+
+}
