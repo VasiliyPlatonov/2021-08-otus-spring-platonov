dfac1e67ad7b9098dea104a731c4af6fe2d43988
Vasiliy Platonov <vasiliyplat@gmail.com>
2022-01-02 20:51:27 +0300

    refact: add a transactional annotation to each method

--- homework6/src/main/java/ru/vasiliyplatonov/homework6/service/BookServiceImpl.java
+++ homework6/src/main/java/ru/vasiliyplatonov/homework6/service/BookServiceImpl.java
@@ -3,8 +3,6 @@ package ru.vasiliyplatonov.homework6.service;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.vasiliyplatonov.homework6.domain.Book;
 import ru.vasiliyplatonov.homework6.domain.Genre;
@@ -13,7 +11,6 @@ import ru.vasiliyplatonov.homework6.repository.BookRepository;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
@@ -21,58 +18,68 @@ public class BookServiceImpl implements BookService {
 
 
 	@Override
+	@Transactional
 	public Book add(Book book) {
 		return bookRepository.add(book);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Book getById(long id) {
 		val book = bookRepository.getById(id);
 		return book.orElse(null);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Book getByIdFullyCompleted(long id) {
 		return bookRepository.getByIdFullyCompleted(id).orElse(null);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Book> getAll() {
 		return bookRepository.getAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Book> getByTitle(String title) {
 		return bookRepository.getByTitle(title);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Book> getByAuthorFirstNameAndLastName(String firstName, String lastName) {
 		return bookRepository.getByAuthorFirstNameAndLastName(firstName, lastName);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Book> getByGenre(Genre genre) {
 		return bookRepository.getByGenre(genre);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Book> getByGenreName(String genreName) {
 		return bookRepository.getByGenreName(genreName);
 	}
 
 	@Override
-	@Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED)
+	@Transactional
 	public void update(Book book) {
 		bookRepository.update(book);
 	}
 
 	@Override
+	@Transactional
 	public void deleteById(long id) {
 		bookRepository.deleteById(id);
 	}
 
 	@Override
+	@Transactional
 	public void deleteByTitle(String title) {
 		bookRepository.deleteByTitle(title);
 	}
